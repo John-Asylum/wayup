7dfdafb3211d21b1656926f3e090a7e05c9dd643
John-Asylum <43915945+John-Asylum@users.noreply.github.com>
2021-03-15 16:54:53 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('.skill').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomIn");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.skill-travel').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomIn");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.skill-create').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomIn");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("flipInY");
+	        }
+	    });
+	});
+})
