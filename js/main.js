e698c60f5efc4091261c73b4a89986751fb751db
John-Asylum <43915945+John-Asylum@users.noreply.github.com>
2021-03-15 16:48:31 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function () {
+	console.log(qwe);
+	$(window).scroll(function() {
+	    $('.skill').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.skill-travel').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomIn");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.skill-create').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomIn");
+	        }
+	    });
+	});
+})
